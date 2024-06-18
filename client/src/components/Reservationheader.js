9facac59b75bb0ce97adbcc7f61a46dd2293d303
Luis Aguilera Garcia <luis.kuniversity@gmail.com>
2024-06-18 14:03:31 -0700

    added link to header

--- client/src/components/Reservationheader.js
+++ client/src/components/Reservationheader.js
@@ -1,57 +1,116 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 import { mobile } from "../responsive";
-import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
-import RedeemIcon from '@mui/icons-material/Redeem';
+import CalendarMonthIcon from "@mui/icons-material/CalendarMonth";
+import RedeemIcon from "@mui/icons-material/Redeem";
 
 const Container = styled.div`
-    height: 45px;
-    background-color: darkorange;
-    color: white;
-    display: flex;
-    justify-content: space-between;
-    font-size: 20px;
-    ${mobile({ fontSize: '17px', height: '65px', textAlign: 'center', paddingBottom: '20px'})}
+  height: 45px;
+  background-color: darkorange;
+  color: white;
+  display: flex;
+  justify-content: space-between;
+  font-size: 20px;
+  ${mobile({
+    fontSize: "17px",
+    height: "65px",
+    textAlign: "center",
+    paddingBottom: "20px",
+  })}
 `;
 
 const ContainerLeft = styled.p`
-    height: 45px;
-    color: white;
-    text-align: left;
-    font-weight: bold;
-    
-    padding-left: 25px;
-    font-size: 20px;
-    ${mobile({ fontSize: '17px', height: '60px', textAlign: 'left', paddingLeft: '10px'})}
+  height: 45px;
+  color: white;
+  text-align: left;
+  font-weight: bold;
+
+  padding-left: 25px;
+  font-size: 20px;
+  ${mobile({
+    fontSize: "17px",
+    height: "60px",
+    textAlign: "left",
+    paddingLeft: "10px",
+  })}
 `;
 
 const ContainerRight = styled.p`
-    height: 45px;
-    color: white;
-    text-align: right !important;
-    padding-right: 25px;
-    font-weight: bold;
-    font-size: 20px;
-    ${mobile({ fontSize: '17px', height: '60px', textAlign: 'right', paddingRight: '10px'})}
+  height: 45px;
+  color: white;
+  text-align: right !important;
+  padding-right: 25px;
+  font-weight: bold;
+  font-size: 20px;
+  ${mobile({
+    fontSize: "17px",
+    height: "60px",
+    textAlign: "right",
+    paddingRight: "10px",
+  })}
 `;
 
 const Reservationheader = () => {
   return (
     <>
-    <Container>
-    <ContainerLeft>
-        Phone : 336-468-2300 | littleheavenlodge@gmail.com
-    </ContainerLeft>
-    <ContainerRight>
-        <RedeemIcon style={{ justifyContent: 'center', fontSize: '35px', padding: '3px', marginRight: '7px'}}/> <a style={{textDecoration: 'none', color: 'white', marginLeft: '10px'}} href="https://secure.thinkreservations.com/littleheavenbedandbreakfast/gift-certificates"> Gift Certificates </a> 
-        |
-        <CalendarMonthIcon style={{ justifyContent: 'center', fontSize: '35px', padding: '3px', marginRight: '7px'}}/> <a style={{textDecoration: 'none', color: 'white', marginLeft: '10px'}} href="https://secure.thinkreservations.com/littleheavenbedandbreakfast/reservations"> Reservations </a> 
-
-    </ContainerRight>
-    </Container>
+      <Container>
+        <ContainerLeft>
+          Phone :{" "}
+          <a href="tel:+13364682300" style={{ color: "white" }}>
+            336-468-2300
+          </a>{" "}
+          |{" "}
+          <a
+            href="mailto:littleheavenlodge@gmail.com"
+            style={{ color: "white" }}
+          >
+            littleheavenlodge@gmail.com
+          </a>
+        </ContainerLeft>
+        <ContainerRight>
+          <RedeemIcon
+            style={{
+              justifyContent: "center",
+              fontSize: "35px",
+              padding: "3px",
+              marginRight: "7px",
+            }}
+          />{" "}
+          <a
+            style={{
+              textDecoration: "none",
+              color: "white",
+              marginLeft: "10px",
+            }}
+            href="https://secure.thinkreservations.com/littleheavenbedandbreakfast/gift-certificates"
+          >
+            {" "}
+            Gift Certificates{" "}
+          </a>
+          |
+          <CalendarMonthIcon
+            style={{
+              justifyContent: "center",
+              fontSize: "35px",
+              padding: "3px",
+              marginRight: "7px",
+            }}
+          />{" "}
+          <a
+            style={{
+              textDecoration: "none",
+              color: "white",
+              marginLeft: "10px",
+            }}
+            href="https://secure.thinkreservations.com/littleheavenbedandbreakfast/reservations"
+          >
+            {" "}
+            Reservations{" "}
+          </a>
+        </ContainerRight>
+      </Container>
     </>
-   
-  )
-}
+  );
+};
 
 export default Reservationheader;
