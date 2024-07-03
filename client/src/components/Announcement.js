3033f392f681df2b21014d3ce084ba9678cae2e0
Luis Aguilera Garcia <luis.kuniversity@gmail.com>
2024-07-02 17:46:05 -0700

    added announcement

--- client/src/components/Announcement.js
+++ client/src/components/Announcement.js
@@ -1,24 +1,29 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 import { mobile } from "../responsive";
 
 const Container = styled.div`
-    height: 40px;
-    background-color: lightgoldenrodyellow;
-    color: black;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    font-size: 20px;
-    ${mobile({padding: '5px', fontSize: '15px', height: '50px', textAlign: 'center'})}
+  height: 40px;
+  background-color: darkorange;
+  color: white;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 20px;
+  ${mobile({
+    padding: "5px",
+    fontSize: "15px",
+    height: "50px",
+    textAlign: "center",
+  })}
 `;
 
 const Announcement = () => {
   return (
     <Container>
-        Get Started Today! Take an additional $40.00 off per room now until March 1st, 2024! Book Today! 
+      Use code JULY and stay 2 nights in July to get $50 off second night.
     </Container>
-  )
-}
+  );
+};
 
 export default Announcement;
